c07690896301df0e6b3acce73e7c337ed8284956
Mohsen Shirazi <mohsensh4792@gmail.com>
2023-04-21 19:37:18 -0400

    Added the focus effect for the name input and functionality for the other role in the job seelct menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-console.log('test');
+const nameInput = document.querySelector('.js-input-name');
+const otherRole = document.querySelector('.js-other-job-role');
+const selectJobRole = document.querySelector('.js-job-role');
+
+
+// Focuses on the name input once the page loads
+nameInput.focus();
+
+// Hides the other job role input initially until the user selects the option from the menu
+otherRole.style.display = 'none';
+
+
+// Shows the other job role input only if the user selects the option
+selectJobRole.addEventListener('change', e => {;
+    if (e.target.value.toLowerCase() === 'other'){
+        otherRole.style.display = 'block';
+    } else {
+        otherRole.style.display = 'none';
+    }
+})
